dd0c86f62d7fb29431d322dedd37e124ad075d37
pepegarciat <jose.garte@gmail.com>
2017-10-01 21:15:06 +0200

    tercera

--- PracticasAula/PracticasAula/src/main/java/es/albarregas/servlets/registro.java
+++ PracticasAula/PracticasAula/src/main/java/es/albarregas/servlets/registro.java
@@ -34,7 +34,7 @@ public class registro extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
+            
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
@@ -59,7 +59,7 @@ public class registro extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        /*processRequest(request, response);*/
     }
 
     /**
@@ -73,18 +73,25 @@ public class registro extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        /*processRequest(request, response);*/
         
         int[] diaMes= {31,28,31,30,31,30,31,31,30,31,30,31};
         response.setContentType("text/html;charset=UTF-8");    
         String nom = request.getParameter("nombre");
         String apel = request.getParameter("apellidos");
         String sexo= request.getParameter("sexo");
-        int dia= Integer.parseInt(request.getParameter("dia"));
-        int mes= Integer.parseInt(request.getParameter("mes"));
-        int anio= Integer.parseInt(request.getParameter("año"));
+        String dia1=request.getParameter("dia");
+        String mes1=request.getParameter("mes");
+        String anio1=request.getParameter("annio");
+        System.out.println(dia1+" "+mes1+" "+anio1);
+        Integer dia= Integer.parseInt(request.getParameter("dia"));
+        System.out.println(dia+"--");
+        Integer mes= Integer.parseInt(request.getParameter("mes"));
+        System.out.println(mes+"--");
+        Integer anio= Integer.parseInt(request.getParameter("annio"));
+        System.out.println(anio+"--");
         String usuario=request.getParameter("usuario");
-        String password=request.getParameter("contraseña");
+        String pass=request.getParameter("contrasena");
         String deporte=request.getParameter("deporte");
         String lectura=request.getParameter("lectura");
         String cine=request.getParameter("cine");
@@ -104,23 +111,27 @@ public class registro extends HttpServlet {
             }
         if (nom.length() ==0){ error=true; }
         if (usuario.length()==0) { error=true; }
-        if (password.length()==0) { error=true; }
+        if (pass.length()==0) { error=true; }
         
         if (error){
                
         try (PrintWriter out = response.getWriter()) {
-            out.println("<!DOCTYPE html>\n"+
-                        "<html>\n"+
-                        "<body>\n"+
-                        "<form>\n"+
-                        "<h2>Errores en el registro</h2\n"+
-                        "<input type=&quotesubmit&quote value=&quotvolver&quot>\n"+
-                        "</form>\n"+
-                        "</body>\n"+
-                        "</html>\n"
+            out.println("<!DOCTYPE html>\n" +
+                        "<html>\n" +
+                        "<body>\n" +
+                        "	<form method=\"POST\">\n" +
+                        "       <h2>Errores en el registro</h2>\n" +
+                        "       <input type=\"submit\" value=\"volver\"/>\n" +
+                        "  </form>\n" +
+                        "  <div>\n" +
+                        "  	<p>prueba esto</p>\n" +
+                        "  </div>\n" +
+                        "</body>\n" +
+                        "</html>"
                     );
-            volver=true;
+           
         }
+         volver=true;
         }
         else { 
             if (volver==true){
@@ -133,7 +144,7 @@ public class registro extends HttpServlet {
                         "<h2>Problemas con el registro</h2\n");
             if (nom.length() ==0){ out.println("<p>El campo nombre es obligatorio</p>");}; 
             if (usuario.length()==0){out.println("<p>El campo usuario es obligatorio</p>");};
-            if (password.length()==0) {out.println("<p>El campo contraseña es obligatorio</p>");};
+            if (pass.length()==0) {out.println("<p>El campo contraseña es obligatorio</p>");};
             if (errorf==true){out.println("<p>Fecha de nacimiento es incorrecta</p>");
             out.println("</div>\n+<br><br>");
             out.println(
@@ -169,7 +180,7 @@ public class registro extends HttpServlet {
             "        <fieldset style=\"border:1px solid blue\">\n" +
             "          <legend style=\"color: blue\">Datos de acceso</legend>\n" +
             "          <label for=\"usuario\">* Usuario: </label> <input type=\"text\" name=\"usuario\" id=\"usuario\" value=\""+usuario+"\">\n" +
-            "          <label for=\"contraseña\"> * Contraseña: </label> <input type=\"password\" name=\"contraseña\" value=\""+password+"\">\n" +
+            "          <label for=\"contraseña\"> * Contraseña: </label> <input type=\"password\" name=\"contraseña\" value=\""+pass+"\">\n" +
             "        </fieldset>\n" +
             "            <br>\n" +
             "          <fieldset style=\"border:1px solid blue\">\n" +
@@ -205,7 +216,7 @@ public class registro extends HttpServlet {
                 "      <p>Sexo: "+sexo+"</p>\n" +
                 "      <p>Fecha de nacimiento: "+dia+" de "+meses[mes-1]+" de "+anio+"</p>\n" +
                 "      <p>Usuario: "+usuario+"</p>\n" +
-                "      <p>Contraseña: "+password+"</p>\n" +
+                "      <p>Contraseña: "+pass+"</p>\n" +
                 "      <p>Preferencias: "+deporte+","+lectura+","+cine+","+viajes+"</p>\n" +
                 "  </body>\n" +
                 "<br><br>\n"+
